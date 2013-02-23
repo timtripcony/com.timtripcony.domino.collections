045d234319cfd2a02eb0d29dd38a9125526306ad
Tim Tripcony <me@timtripcony.com>
2013-02-23 14:08:07 -0500

    cleaner collection recycling

--- com.timtripcony.domino.collections/Code/Java/com/timtripcony/util/DominoUtil.java
+++ com.timtripcony.domino.collections/Code/Java/com/timtripcony/util/DominoUtil.java
@@ -1,6 +1,9 @@
 package com.timtripcony.util;
 
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 import lotus.domino.Base;
 
@@ -10,8 +13,16 @@ public class DominoUtil {
 		for (Object object : objects) {
 			if (object != null) {
 				if (object instanceof Collection) {
-					for (Object eachMember : (Collection) object) {
-						incinerate(eachMember);
+					if (object instanceof Map) {
+						Set<Map.Entry> entries = ((Map) object).entrySet();
+						for (Map.Entry<?, ?> entry : entries) {
+							incinerate(entry.getKey(), entry.getValue());
+						}
+					} else {
+						Iterator i = ((Collection) object).iterator();
+						while (i.hasNext()) {
+							incinerate(i.next());
+						}
 					}
 				} else if (object instanceof Base) {
 					Base dominoObject = (Base) object;
