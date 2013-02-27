a5ea97cfdc0c34de7b2e314f1686a974ad34df63
Tim Tripcony <me@timtripcony.com>
2013-02-27 06:56:01 -0500

    Fixed bug where ViewEntryIterator would always skip the first entry, and added licensing information to all classes

--- com.timtripcony.domino.collections/Code/Java/com/timtripcony/domino/collections/ViewEntryIterator.java
+++ com.timtripcony.domino.collections/Code/Java/com/timtripcony/domino/collections/ViewEntryIterator.java
@@ -1,3 +1,18 @@
+/*
+ * © Copyright Tim Tripcony 2013
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at:
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+ * implied. See the License for the specific language governing 
+ * permissions and limitations under the License.
+ */
 package com.timtripcony.domino.collections;
 
 import lotus.domino.ViewEntry;
@@ -10,6 +25,8 @@ import com.timtripcony.util.LogUtil;
 public class ViewEntryIterator extends AbstractDominoIterator<ViewEntry> {
 	private transient ViewEntryCollection collection_;
 	private transient ViewEntry currentEntry_;
+	private boolean started_;
+	private boolean done_;
 
 	protected ViewEntryIterator(ViewEntryCollection collection) {
 		setCollection(collection);
@@ -20,31 +37,31 @@ public class ViewEntryIterator extends AbstractDominoIterator<ViewEntry> {
 	}
 
 	public ViewEntry getCurrentEntry() {
-		if (currentEntry_ == null) {
-			try {
-				currentEntry_ = getCollection().getFirstEntry();
-			} catch (Throwable t) {
-				LogUtil.trace(t, "Error getting first entry from collection");
-			}
-		}
 		return currentEntry_;
 	}
 
 	public boolean hasNext() {
 		boolean result = false;
 		ViewEntry currentEntry = getCurrentEntry();
-		if (currentEntry != null) {
-			ViewEntry nextEntry = null;
-			try {
-				nextEntry = getCollection().getNextEntry(currentEntry);
-				result = (nextEntry != null);
-			} catch (Throwable t) {
-				LogUtil.trace(t, "Error seeking a next entry");
-			} finally {
-				DominoUtil.incinerate(nextEntry);
-			}
+		ViewEntry nextEntry = null;
+		try {
+			nextEntry = ((currentEntry == null) ? (isDone() ? null : getCollection().getFirstEntry()) : getCollection().getNextEntry(
+					currentEntry));
+			result = (nextEntry != null);
+		} catch (Throwable t) {
+			LogUtil.trace(t, "Error seeking a next entry");
+		} finally {
+			DominoUtil.incinerate(nextEntry);
 		}
 		return result;
+	}
+
+	public boolean isDone() {
+		return done_;
+	}
+
+	public boolean isStarted() {
+		return started_;
 	}
 
 	public ViewEntry next() {
@@ -52,7 +69,7 @@ public class ViewEntryIterator extends AbstractDominoIterator<ViewEntry> {
 		ViewEntry result = null;
 		ViewEntry currentEntry = getCurrentEntry();
 		try {
-			result = getCollection().getNextEntry(currentEntry);
+			result = ((currentEntry == null) ? getCollection().getFirstEntry() : getCollection().getNextEntry(currentEntry));
 		} catch (Throwable t) {
 			LogUtil.trace(t, "Error getting next view entry from collection");
 		} finally {
@@ -73,5 +90,15 @@ public class ViewEntryIterator extends AbstractDominoIterator<ViewEntry> {
 
 	public void setCurrentEntry(ViewEntry currentEntry) {
 		currentEntry_ = currentEntry;
+		setStarted(currentEntry != null);
+		setDone(currentEntry == null);
+	}
+
+	public void setDone(boolean done) {
+		done_ = done;
+	}
+
+	public void setStarted(boolean started) {
+		started_ = started;
 	}
 }
